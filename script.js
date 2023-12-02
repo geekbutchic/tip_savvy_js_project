ec565a0ba146269775dbcf0c3692f5849f4e9f35
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-12-01 18:23:34 -0700

    adds tip display, tip total display, bill input, tip%, functionality, next feature is party size functinality

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 const toggleSwitch = document.getElementById("toggle");
 const body = document.body;
 
+// Tip & Total Selectors * 2
 const tipDisplay = document.querySelector(".tip-section span");
 const totalDisplay = document.querySelector(".total-section span");
+// Input Selectors * 3
+const billAmount = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const guestsInput = document.getElementById("people");
 
 // Light & Dark Mode
 toggleSwitch.addEventListener("change", () => {
@@ -19,5 +24,24 @@ toggleSwitch.addEventListener("change", () => {
   }
 });
 
-tipDisplay.textContent = 1;
-totalDisplay.textContent = 1;
+// Starting Values
+billAmount.value = 0;
+tipInput.value = 15;
+guestsInput.value = 1;
+
+function calculateTipTotal() {
+  // Get the values from input fields
+  const bill = parseFloat(billAmount.value);
+  const tipPercentage = parseFloat(tipInput.value);
+  // Calculate Tip and Total
+  const tipAmount = bill * tipPercentage / 100;
+  const totalBill = bill + tipAmount;
+  //Update Display
+  tipDisplay.textContent = tipAmount.toFixed(2);
+  totalDisplay.textContent = totalBill.toFixed(2);
+}
+
+billAmount.addEventListener("input", calculateTipTotal);
+tipInput.addEventListener("input", calculateTipTotal);
+
+calculateTipTotal();
